ca5e3d18e17c47e87d816e38ae4971446b91d447
Victor Reyes Medina <victor.reyes.medina@gmail.com>
2019-06-07 01:45:42 -0400

    Add models an database context

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(DataContext context)
+        {
+            if (context.Database.EnsureCreated())
+            { // Database didn't exists
+            }
+
+
+        }
+
+    }
+}
